21c9c6e89d951c98fef57556fbd1cb622cab2d47
alhade1988 <alhade1988@hotmail.com>
2022-07-04 02:44:27 +0200

    FE21-CR4-Abdul Hadi

---
+++ index.js
@@ -0,0 +1,36 @@
+let movisjson = JSON.parse(movis);
+
+
+for (let val of movisjson) {
+    document.getElementById("result").innerHTML += `
+    <div class="row-cols-1 row-cols-md-2 row-cols-lg-3">
+    <div class="card" style="width: 19rem;">
+    <img src="${val.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${val.movieName}</h5>
+      <p class="card-text ">${val.description}.</p>
+      <a  class="btn btn-primary likes">likes<samp class="result">${val.likes}</samp></a>
+    </div>
+  </div>
+  </div>`;
+
+
+
+}
+
+let btns = document.getElementsByClassName("likes");
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        movisjson[i].likes++;
+
+        document.getElementsByClassName("result")[i].innerHTML = movisjson[i].likes;
+
+
+    })
+}
+let btns1 = document.getElementById("sort");
+btns1.addEventListener("click", function() {
+    movisjson.sort((a, b) => a.likes - b.likes);
+    console.table(movisjson)
+})
