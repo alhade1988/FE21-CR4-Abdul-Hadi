396ad4a9feea094bd83769db1007b14ed4ede9ee
alhade1988 <alhade1988@hotmail.com>
2022-08-05 16:55:59 +0200

    add

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let movisjson = JSON.parse(movis);
 
-for (let val of movisjson) {
-    document.getElementById("result").innerHTML += `
+function Add() {
+    document.getElementById("result").innerHTML = "";
+    for (let val of movisjson) {
+        document.getElementById("result").innerHTML += `
     <div class="row-cols-1 row-cols-md-2 row-cols-lg-3">
     <div class="card" style="width: 19rem;">
     <img src="${val.image}" class="card-img-top" alt="...">
@@ -15,51 +17,35 @@ for (let val of movisjson) {
 
 
 
-}
-
-
-
-
-let btns = document.getElementsByClassName("likes");
-
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        movisjson[i].likes++;
-
-        document.getElementsByClassName("result")[i].innerHTML = movisjson[i].likes;
-
-
-    })
-}
-
-
-let btns1 = document.getElementById("sort");
-btns1.addEventListener("click", function() {
-    movisjson.sort((a, b) => b.likes - a.likes);
-
-
-
-    for (let val of movisjson) {
-        document.getElementById("result").innerHTML += `
-          <div class="row-cols-1 row-cols-md-2 row-cols-lg-3">
-          <div class="card" style="width: 19rem;">
-          <img src="${val.image}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${val.movieName}</h5>
-            <p class="card-text ">${val.description}.</p>
-            <a  class="btn btn-primary likes">likes<samp class="result">${val.likes}</samp></a>
-          </div>
-        </div>
-        </div>`;
-
-
     }
+    addLikes();
+}
 
 
 
-    console.table(movisjson)
+function addLikes() {
+    let btns = document.getElementsByClassName("likes");
+
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+            movisjson[i].likes++;
+
+            document.getElementsByClassName("result")[i].innerHTML = movisjson[i].likes;
+
+
+        })
+    }
+}
+Add(movisjson);
+
+
+function sort() {
+    movisjson.sort((a, b) => {
+        return b.likes - a.likes;
+    });
+}
+document.getElementById("sort").addEventListener("click", sort);
+document.getElementById("sort").addEventListener("click", function() {
+    sort();
+    Add(movisjson);
 })
-
-
-
-// for (let val of movisbtjson)
